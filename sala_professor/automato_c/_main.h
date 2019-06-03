e41b10d37d9eb6dc7cbe0c91cf6290de87939747
jlsjohn <jonathas_silva14@yahoo.com.br>
2019-06-03 11:42:36 -0300

    Inserindo lógica para lâmpada e botão desliga geral

--- sala_professor/automato_c/_main.h
+++
@@ -1,9 +0,0 @@
-/* --- Generated the 6/4/2019 at 1:54 --- */
-/* --- heptagon compiler, version 1.05.00 (compiled wed. mar. 13 20:25:55 CET 2019) --- */
-/* --- Command line: /home/labcontrol/.opam/default/bin/heptc -target c -target z3z -s duas_tarefas_concorrentes automato.ept --- */
-
-#ifndef _MAIN_H
-#define _MAIN_H
-
-#include "automato.h"
-#endif // _MAIN_H
--- sala_professor/duas_tarefas_concorrentes_controller_c/duas_tarefas_concorrentes_controller_types.c
+++
@@ -1,9 +0,0 @@
-/* --- Generated the 6/4/2019 at 1:55 --- */
-/* --- heptagon compiler, version 1.05.00 (compiled wed. mar. 13 20:25:55 CET 2019) --- */
-/* --- Command line: /home/labcontrol/.opam/default/bin/heptc -target c duas_tarefas_concorrentes_controller.ept --- */
-
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include "duas_tarefas_concorrentes_controller_types.h"
-
